0fe8ab0b19522e7e39737d549ec1e60d7cfda046
AnnaLebedeva1001 <lebeda.10012002@gmail.com>
2023-02-26 16:47:08 +0300

    реализован LRUCache с помощью OrderedDict

---
+++ lab1/cache.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int=10) -> None:
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: str) -> str:
+        if key not in self.cache:
+            return ''
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def set(self, key: str, value: str) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
+
+    def rem(self, key: str) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
